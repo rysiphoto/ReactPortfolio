0ea3fbe642e3a865864705944caff81f92369f05
Ryan Siverson <rcsskier@mac.com>
2020-05-29 10:57:11 -0600

    Updates

---
+++ precache-manifest.8f3ee35c0ad7bf20dd32256f3c883136.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06a719907af1b5f35dfb77145881a868",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "d45f70e2a13a6f83a849",
+    "url": "/ReactPortfolio/static/css/main.9cec8658.chunk.css"
+  },
+  {
+    "revision": "1e71d8c7f0a97d6aaf28",
+    "url": "/ReactPortfolio/static/js/2.182b61d6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactPortfolio/static/js/2.182b61d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d45f70e2a13a6f83a849",
+    "url": "/ReactPortfolio/static/js/main.95a9e001.chunk.js"
+  },
+  {
+    "revision": "56afbf374bd24c6c2ff7",
+    "url": "/ReactPortfolio/static/js/runtime-main.c68f812c.js"
+  }
+]);
