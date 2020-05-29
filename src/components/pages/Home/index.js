46d8d8dc92b4f3b5d6733584afc23470250e3591
Ryan Siverson <rcsskier@mac.com>
2020-05-29 10:01:41 -0600

    Home page transfered over

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -3,18 +3,28 @@ import "./style.css";
 
 function Home() {
   return (
-    <div>
-      <h1>Ryan Siverson</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque velit, lobortis ut magna
-        varius, blandit rhoncus sem. Morbi lacinia nisi ac dui fermentum, sed luctus urna tincidunt.
-        Etiam ut feugiat ex. Cras non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna
-        imperdiet ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras rutrum
-        ligula in tincidunt commodo. Morbi sit amet mollis orci, in tristique ex. Donec nec ornare
-        elit. Donec blandit est sed risus feugiat porttitor. Vestibulum molestie hendrerit massa non
-        consequat. Vestibulum vitae lorem tortor. In elementum ultricies tempus. Interdum et
-        malesuada fames ac ante ipsum primis in faucibus.
-      </p>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8">
+          <h1>About Me</h1>
+          <hr />
+          <img src="assets/Headshot.jpg" id="photo" class="rounded float-left" height="150px" width="150px"
+            alt="Ryan Siverson" />
+          <p>
+            I am a student at Denver University, currently enrolled in their twelve week Full Stack Web
+            Development program.
+                    On my <a href="portfolio.html">Portfolio</a> page, I have added a link to my updated resume, first
+                    group project, two class assignments
+                    and my GitHub repository. If you would like to contact me, check out the <a
+              href="contact.html">Contact</a> page!
+                </p>
+
+
+        </div>
+        <div class="col-md-4">
+
+        </div>
+      </div>
     </div>
   );
 }
