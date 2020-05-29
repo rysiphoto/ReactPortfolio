0ca3ece233fbb06fe55595057978fad402e9b053
Ryan Siverson <rcsskier@mac.com>
2020-05-29 11:18:16 -0600

    fixing react deployment

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <div>
         <NavTabs />
-        <Route exact path="/" component={Home} />
+        <Route path="/" component={Home} />
         <Route exact path="/portfolio" component={Portfolio} />
         <Route path="/contact" component={Contact} />
         <Footer />
