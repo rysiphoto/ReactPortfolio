6144f4c45dede4e6c00b1170f4abeddd19f377ad
Ryan Siverson <rcsskier@mac.com>
2020-08-12 21:59:17 -0600

    build folder

---
+++ build/precache-manifest.beddca7dec901ae83abd2ef8dc9abdb2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc8d21ee26310ffaa8651a222edfddf8",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "b312d94439b3344d2853",
+    "url": "/ReactPortfolio/static/css/main.b03d5b93.chunk.css"
+  },
+  {
+    "revision": "bd83776d9f329107bbe0",
+    "url": "/ReactPortfolio/static/js/2.929a9f11.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactPortfolio/static/js/2.929a9f11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b312d94439b3344d2853",
+    "url": "/ReactPortfolio/static/js/main.57b4c516.chunk.js"
+  },
+  {
+    "revision": "56afbf374bd24c6c2ff7",
+    "url": "/ReactPortfolio/static/js/runtime-main.c68f812c.js"
+  },
+  {
+    "revision": "f6322a8cf22927c6d6ee726f066fb093",
+    "url": "/ReactPortfolio/static/media/BF_screenshot.f6322a8c.png"
+  },
+  {
+    "revision": "4460f8d8ef37355488e6db2dd830c21f",
+    "url": "/ReactPortfolio/static/media/Github.4460f8d8.png"
+  },
+  {
+    "revision": "8a1763c8c9788ecd12d450cd2a41cb0c",
+    "url": "/ReactPortfolio/static/media/Headshot.8a1763c8.jpg"
+  },
+  {
+    "revision": "4f81aaa0d223f6175f84437e4af36b30",
+    "url": "/ReactPortfolio/static/media/ale.4f81aaa0.png"
+  },
+  {
+    "revision": "c3763b82bf3aa284c06e70dea6f5c593",
+    "url": "/ReactPortfolio/static/media/bg.c3763b82.jpg"
+  },
+  {
+    "revision": "42a1924c4046c7bc7c73af4011763ab0",
+    "url": "/ReactPortfolio/static/media/eattb.42a1924c.png"
+  },
+  {
+    "revision": "0c4930f8557456d503a33ed5fb8dc41f",
+    "url": "/ReactPortfolio/static/media/mountains.0c4930f8.jpg"
+  },
+  {
+    "revision": "44d3c25013cc69f9fdb9401326a0adca",
+    "url": "/ReactPortfolio/static/media/notetaker.44d3c250.png"
+  }
+]);
